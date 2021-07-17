5f362ecc0d84f2b002d3522395f26e664ddb5004
Laura Corkin <nzlaura.williams@gmail.com>
2021-07-18 10:37:51 +1200

    created functions in apiclient for handling adding/displaying events

--- client/apis/apiClient.js
+++ client/apis/apiClient.js
@@ -1,5 +1,6 @@
 import request from 'superagent'
 const contactUrl = '/api/v1/contact/'
+const rootUrl = 'api/v1'
 
 export function sendContactFormMessage (contactForm) {
   return request.post(contactUrl)
@@ -9,3 +10,38 @@ export function sendContactFormMessage (contactForm) {
     })
 }
 // add error handling
+
+// getting calendar events
+
+export function getEvents () {
+  return request.get(rootUrl + '/calendar')
+    .then(res => {
+      return res.body
+    })
+    .catch(logError)
+}
+
+export function createEvent (event) {
+  return request
+    .post(rootUrl + 'events')
+    .send({ event })
+    .then(res => {
+      return res.body
+    })
+    .catch(logError)
+}
+
+// error function
+
+function logError (err) {
+  if (err.message === 'Forbidden') {
+    throw new Error('You cannot do that')
+  } else {
+    // eslint-disable-next-line no-console
+    console.error(
+      'Error consuming the API (in apis/apiClient.js):',
+      err.message
+    )
+    throw err
+  }
+}
