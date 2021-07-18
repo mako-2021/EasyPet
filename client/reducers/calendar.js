f30b60e23979a940b62b52cdf4f53d8198737f01
Laura Corkin <nzlaura.williams@gmail.com>
2021-07-18 13:32:43 +1200

    created routes for fetching and saving events

--- client/reducers/calendar.js
+++ client/reducers/calendar.js
@@ -1,12 +1,12 @@
 const initialState = []
 
-export default funtuon (state = initialState, action) {
-    const {id, event} = action
+export default function (state = initialState, action) {
+  const { id, event } = action
 
-    switch(action.type) {
-        case 'ADD_EVENT':
-            return [...state, { id, event}]
-            default:
-                return state
-    }
+  switch (action.type) {
+    case 'ADD_EVENT':
+      return [...state, { id, event }]
+    default:
+      return state
+  }
 }
---
+++ server/routes/events.js
@@ -0,0 +1,31 @@
+const express = require('express')
+
+const router = express.Router()
+
+// TODO: write get events function
+
+router.get('/', (req, res) => {
+  getEvents()
+    .then(results => {
+      res.json(results)
+      return null
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({ message: 'Oops, something went wrong' })
+    })
+})
+
+// TODO: add save event function also
+
+router.post('/', (req, res) => {
+  saveEvent(req.body.event)
+    .then(ids => {
+      res.status(201).json(ids[0])
+      return null
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({ message: 'Oops, something went wrong' })
+    })
+})
